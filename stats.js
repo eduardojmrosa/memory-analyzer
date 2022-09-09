6e6e17cd464a09ce7895b921d121442bebc6b0cf
eduardojmrosa <eduardojose222002@hotmail.com>
2022-09-09 12:50:33 -0300

    realtime-update

--- stats.js
+++ stats.js
@@ -1,6 +1,7 @@
 const os = require ('os')
 
 setInterval(()=>{
+
 const {freemem, totalmem }= os
 const  mem = parseInt(freemem() / 1024 / 1024)
 const totalMem = parseInt(totalmem() / 1024 / 1024) 
@@ -13,5 +14,5 @@ const stats = {
 console.clear()
 console.log("---PC STATS---")
 console.table(stats)
-
 }, 1000)
+.
